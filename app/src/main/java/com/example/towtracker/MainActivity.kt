45d811c9b7d53c09a33cf7f353daef7676cb7f4d
mayonnaise <90057279+Mayanzev@users.noreply.github.com>
2024-11-25 02:08:47 +0300

    add bottomNavigationView

--- app/src/main/java/com/example/towtracker/MainActivity.kt
+++ app/src/main/java/com/example/towtracker/MainActivity.kt
@@ -9,7 +9,6 @@ import com.example.towtracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
