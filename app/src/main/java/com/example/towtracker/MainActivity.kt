52ec578b35236536081142091666e664a6a8586e
mayonnaise <90057279+Mayanzev@users.noreply.github.com>
2024-11-25 03:00:17 +0300

    add bottomMenu

--- app/src/main/java/com/example/towtracker/MainActivity.kt
+++ app/src/main/java/com/example/towtracker/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.towtracker
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,9 +10,23 @@ import com.example.towtracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        onBottomNavClick()
     }
+
+    private fun onBottomNavClick() {
+        binding.bNan.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.id_home -> Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                R.id.id_tracks -> Toast.makeText(this, "Tracks", Toast.LENGTH_SHORT).show()
+                R.id.id_settings -> Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+
 }
