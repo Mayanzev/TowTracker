c5bae56e6415fb4e2cd3793f5b09f23c576d15da
mayonnaise <90057279+Mayanzev@users.noreply.github.com>
2024-11-26 00:00:56 +0300

    add color preference change listeners

--- app/src/main/java/com/example/towtracker/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/towtracker/fragments/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.towtracker.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
@@ -8,6 +9,7 @@ import com.example.towtracker.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
     private lateinit var timePref: Preference
+    private lateinit var colorPref: Preference
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.main_preferences, rootKey)
@@ -16,21 +18,30 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     private fun init() {
         timePref = findPreference("update_time_key")!!
+        colorPref = findPreference("color_key")!!
         val changeListener = onChangeListener()
         timePref.onPreferenceChangeListener = changeListener
+        colorPref.onPreferenceChangeListener = changeListener
         initPrefs()
     }
 
     private fun onChangeListener(): Preference.OnPreferenceChangeListener {
         return Preference.OnPreferenceChangeListener {
             pref, value ->
-            val nameArray = resources.getStringArray(R.array.loc_time_update_name)
-            val valueArray = resources.getStringArray(R.array.loc_time_update_value)
-            val title = pref.title.toString().substringBefore(":")
-            val pos = valueArray.indexOf(value)
-            pref.title = "$title: ${nameArray[pos]}"
+                when (pref.key) {
+                    "update_time_key" -> onTimeChange(value.toString())
+                    "color_key" -> pref.icon?.setTint(Color.parseColor(value.toString()))
+                }
             true
         }
+    }
+
+    private fun onTimeChange(value: String) {
+        val nameArray = resources.getStringArray(R.array.loc_time_update_name)
+        val valueArray = resources.getStringArray(R.array.loc_time_update_value)
+        val title = timePref.title.toString().substringBefore(":")
+        val pos = valueArray.indexOf(value)
+        timePref.title = "$title: ${nameArray[pos]}"
     }
 
     private fun initPrefs() {
@@ -40,5 +51,8 @@ class SettingsFragment : PreferenceFragmentCompat() {
         val title = timePref.title
         val pos = valueArray.indexOf(pref?.getString("update_time_key", "3000"))
         timePref.title = "$title: ${nameArray[pos]}"
+
+        val trackColor = pref?.getString("color_key", "#0077FF")
+        colorPref.icon?.setTint(Color.parseColor(trackColor))
     }
 }
