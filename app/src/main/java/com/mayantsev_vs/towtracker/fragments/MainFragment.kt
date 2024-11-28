70f3ff771a2447eeffb63cf1dda15c1b015e0f75
mayonnaise <90057279+Mayanzev@users.noreply.github.com>
2024-11-29 00:24:48 +0300

    add functions to request location permissions

--- app/src/main/java/com/mayantsev_vs/towtracker/fragments/MainFragment.kt
+++ app/src/main/java/com/mayantsev_vs/towtracker/fragments/MainFragment.kt
@@ -1,18 +1,29 @@
 package com.mayantsev_vs.towtracker.fragments
 
+import android.Manifest
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import com.mayantsev_vs.towtracker.databinding.FragmentMainBinding
+import com.mayantsev_vs.towtracker.utils.checkPermission
+import com.mayantsev_vs.towtracker.utils.showToast
 import org.osmdroid.config.Configuration
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class MainFragment : Fragment() {
+    private lateinit var pLauncher: ActivityResultLauncher<Array<String>>
     private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
@@ -26,10 +37,11 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initOSM()
+        registerPermissions()
+        checkLocPermission()
     }
 
-    private fun settingsOsm () {
+    private fun settingsOsm() {
         Configuration.getInstance().load(
             activity as ComponentActivity,
             activity?.getSharedPreferences("osm_pref", Context.MODE_PRIVATE)
@@ -47,6 +59,92 @@ class MainFragment : Fragment() {
         mLocOverlay.runOnFirstFix {
             map.overlays.clear()
             map.overlays.add(mLocOverlay)
+        }
+    }
+
+    private fun registerPermissions() {
+        pLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            if (permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true ||
+                permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true) {
+                initOSM()
+            } else {
+                showToast("Вы не дали разрешения на использование местоположения!")
+            }
+        }
+    }
+
+    private fun checkLocPermission() {
+        when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> checkPermissionAfter11() // Android 11 и выше
+            Build.VERSION.SDK_INT == Build.VERSION_CODES.Q -> checkPermissionAfter10() // Android 10
+            else -> checkPermission10() // Android 9 и ниже
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    private fun checkPermissionAfter11() {
+        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION) ||
+            checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            initOSM()
+            if (!checkPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+                showBackgroundPermissionDialog()
+            }
+        } else {
+            pLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
+    }
+
+    private fun showBackgroundPermissionDialog() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri: Uri = Uri.fromParts("package", requireActivity().packageName, null)
+        intent.data = uri
+        val dialog = android.app.AlertDialog.Builder(requireContext())
+            .setTitle("Разрешение на фоновое местоположение")
+            .setMessage("Для улучшения работы приложения нам нужно доступ к вашему местоположению даже в фоновом режиме. Пожалуйста, разрешите это в настройках приложения.")
+            .setPositiveButton("Перейти в настройки") { _, _ ->
+                startActivity(intent)
+            }
+            .setNegativeButton("Отмена", null)
+            .create()
+
+        dialog.show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun checkPermissionAfter10() {
+        if ((checkPermission(Manifest.permission.ACCESS_FINE_LOCATION) ||
+                    checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION)) &&
+            checkPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+            initOSM()
+        } else {
+            pLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                )
+            )
+        }
+    }
+
+    private fun checkPermission10() {
+        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION) ||
+            checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            initOSM()
+        } else {
+            pLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
         }
     }
 
