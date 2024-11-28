c13fa6320a9d38621e4ae691f10afc75cb919036
mayonnaise <90057279+Mayanzev@users.noreply.github.com>
2024-11-29 00:58:05 +0300

    add location enable dialog

--- app/src/main/java/com/mayantsev_vs/towtracker/fragments/MainFragment.kt
+++ app/src/main/java/com/mayantsev_vs/towtracker/fragments/MainFragment.kt
@@ -15,8 +15,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.mayantsev_vs.towtracker.databinding.FragmentMainBinding
+import com.mayantsev_vs.towtracker.utils.DialogManager
 import com.mayantsev_vs.towtracker.utils.checkPermission
 import com.mayantsev_vs.towtracker.utils.showToast
 import org.osmdroid.config.Configuration
@@ -161,7 +163,7 @@ class MainFragment : Fragment() {
         val lManager = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         val isEnabled = lManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
         if (!isEnabled) {
-            showToast("Location not enabled")
+            DialogManager.showLocEnableDialog(activity as AppCompatActivity)
         } else {
             showToast("Location enabled")
         }
---
+++ app/src/main/java/com/mayantsev_vs/towtracker/utils/DialogManager.kt
@@ -0,0 +1,22 @@
+package com.mayantsev_vs.towtracker.utils
+
+import android.app.AlertDialog
+import android.content.Context
+import android.widget.Toast
+import com.mayantsev_vs.towtracker.R
+
+object DialogManager {
+    fun showLocEnableDialog(context: Context) {
+        val builder = AlertDialog.Builder(context)
+        val dialog = builder.create()
+        dialog.setTitle(R.string.location_disabled)
+        dialog.setMessage(context.getString(R.string.location_dialog_message))
+        dialog.setButton(AlertDialog.BUTTON_POSITIVE, "Yes") {
+            _, _ -> Toast.makeText(context, "Yes", Toast.LENGTH_SHORT).show()
+        }
+        dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "No") {
+                _, _ -> Toast.makeText(context, "No", Toast.LENGTH_SHORT).show()
+        }
+        dialog.show()
+    }
+}
